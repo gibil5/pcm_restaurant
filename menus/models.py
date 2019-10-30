b07f7c66498101df302f191973bd3a840da38e99
gibil <jrevilla55@gmail.com>
2019-10-30 15:33:04 -0500

    ..

--- menus/models.py
+++ menus/models.py
@@ -97,12 +97,13 @@ class HotDrink(Item):
 
 class Menu(models.Model):
 
-	menu_title = models.CharField(max_length=200)
+	#menu_title = models.CharField(max_length=200)
+	title = models.CharField(max_length=200)
 
 	date = models.DateTimeField('date')
 
 	def __str__(self): 
-		return self.menu_title
+		return self.title
 
 
 	def entries_empty(self):
