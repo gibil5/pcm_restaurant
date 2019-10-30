0e57e1c05d68642cccbedf11d2d79a0b38aa12be
gibil <jrevilla55@gmail.com>
2019-10-30 17:58:33 -0500

    ..

--- pcm/settings.py
+++ pcm/settings.py
@@ -146,9 +146,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
+
+# Heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+
+
+# Btre
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = [
+#                    os.path.join(BASE_DIR, 'pcm/static'),
+#]
+
+
+
+
 
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
