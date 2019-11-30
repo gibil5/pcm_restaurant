56966d91ab0eeb61f2d1ad962a42effd8971d7f5
gibil <jrevilla55@gmail.com>
2019-11-30 13:42:42 -0500

    ..

--- menus/tests.py
+++ menus/tests.py
@@ -15,6 +15,7 @@ Tests:
 """
 
 from django.test import TestCase
+
 from django.test import Client
 from django.utils import timezone
 from django.urls import reverse
@@ -28,8 +29,26 @@ _MSG_ERROR_EMPTY = "No existe ningún Menú todavía."
 
 
 
-# Test Menu Views Index - Empty
-class MenuEmptyTests(TestCase):
+# Test Model View - Empty
+#class MenuEmptyTests(TestCase):
+class ModelEmptyTests(TestCase):
+
+	#def __init__(self):
+	#	print()
+	#	print('MenuEmptyTests')
+
+
+	def setUp(self):
+		print()
+		print('setup - ModelEmptyTests')
+		
+		self.name = 'ModelEmptyTests'
+		
+		#self.password = 'boris_the_sneaky_russian'
+		#self.client = Client()  # Django's TestCase already sets self.client so this line isn't required
+		#self.login_status = self.createUserAndLogin(user_name, password)
+
+
 
 
 	# Test 1
@@ -38,21 +57,21 @@ class MenuEmptyTests(TestCase):
 		Test Index View with no Objects
 		"""
 		print()
-		print('Test Menu 1: Begin')
-		print('Index View. No Menus')
+		print('\ttest_index_empty : Begin')
+
+		print(self.name)
+		#print(self.password)
 
 
 		# Create Client 
 		c = Client()
 		
-		path = 'menus'
-		
-		lib.test_status_code_ok(self, c, path)
-
 
 
 		# Get Response to /menus/
 		#response = c.post('/menus/')
+		response = c.post(self.path_index)
+
 		#print(response)
 
 
@@ -60,7 +79,7 @@ class MenuEmptyTests(TestCase):
 		# Test1 - Check Status Code - Response OK
 		#print()
 		#print('\tCheck Status Code')
-		#self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, 200)
 
 
 
@@ -68,16 +87,45 @@ class MenuEmptyTests(TestCase):
 		#print()
 		#print('\tCheck Page Empty Message')
 		#self.assertContains(response, _MSG_ERROR_EMPTY)
+		self.assertContains(response, self.msg_error_index_empty)
 
 
 		# Test 3 - Check Context
 		#print()
 		#print('\tCheck Context')
 		#self.assertQuerysetEqual(response.context["menus"], [])
+		self.assertQuerysetEqual(response.context[self.ctx_0_name], [])
 
 
 		print()
-		print('Test Menu 1: End')
+		print('\ttest_index_empty: End')
+
+
+
+
+
+# Test Menu Views Index - Empty
+class MenuEmptyTests(ModelEmptyTests):
+
+	def setUp(self):
+		print()
+		print('setup - MenuEmptyTests')
+		
+		self.name = 'MenuEmptyTests'
+
+		self.path_index = '/menus/'
+
+		self.msg_error_index_empty = "No existe ningún Menú todavía."
+
+		self.ctx_0_name = "menus"
+		
+		#self.ctx_0_value = False
+
+
+
+	def __str__(self):
+		return self.name
+
 
 
 
@@ -245,7 +293,7 @@ class MenuUpdateTests(TestCase):
 
 
 
-        #self.assertEqual(len(response.context['customers']), 5)
+		#self.assertEqual(len(response.context['customers']), 5)
 
 
 		#self.assertQuerysetEqual(
